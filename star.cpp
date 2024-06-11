e5600b4b34246c20a49fba34c9912810c2aa8e96
pawankr9060 <148863359+pawankr9060@users.noreply.github.com>
2024-06-11 11:42:56 +0530

    Initial Commit

---
+++ star.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+using namespace std;
+
+int main()
+{
+    int i, j, n;
+
+
+    printf("Enter number : ");
+    scanf("%d", &n);
+
+    for(i=n; i<=1; i++)
+    {
+        for(j=1;j>=i;j--)
+        {
+            printf("o ");
+        }
+        printf("\n"); 
+    }
+    return 0;
+}
+
